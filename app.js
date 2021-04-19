4d5f4e0ecbc9a2fe4197cea20beea779f4d934a5
Maia <maiafleschler@gmail.com>
2021-04-19 14:27:51 -0300

    Agrega edicion de imagen

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const closeImg = document.querySelector("#closeImg");
 const closeText = document.querySelector("#closeText");
 const imageEdit = document.querySelector(".imageEdit");
 const textEdit = document.querySelector(".textEdit");
+const divImg = document.querySelector(".imgMeme");
+const inputUrl = document.querySelector("#url");
+const backgroundColor = document.querySelector("#backgroundColor");
+const backgroundColorNumber = document.querySelector(".backgroundColorNumber");
+const backgroundOptions = document.querySelector("#backgroundOptions");
+const brightness = document.querySelector("#brightness");
+const opacity = document.querySelector("#opacity");
+const contrast = document.querySelector("#contrast");
+const blur_ = document.querySelector("#blur");
+const grayscale = document.querySelector("#grayscale");
+const sepia = document.querySelector("#sepia");
+const hue = document.querySelector("#hue");
+const saturation = document.querySelector("#saturation");
+const negative = document.querySelector("#negative");
+const resetFilters = document.querySelector(".resetFilters");
 
 // Abrir y cerrar paneles
 
@@ -55,4 +70,66 @@ function switchTheme(e) {
     document.documentElement.setAttribute('data-theme', targetTheme)
     }
     switchThemeButton.addEventListener('click', switchTheme);
+
+// Edicion imagen
+
+// Agrego imagen
+inputUrl.addEventListener("keyup",()=>{
+    const url = inputUrl.value;
+    divImg.style.backgroundImage = `url(${url})`;
+});
+
+// Cambio color de fondo
+backgroundColor.addEventListener("input",()=>{
+    const color = backgroundColor.value;
+    divImg.style.backgroundColor = color;
+    backgroundColorNumber.textContent = color;
+});
+
+// Blend Mode
+backgroundOptions.addEventListener('change', (event) => {
+    divImg.style.backgroundBlendMode = event.target.value;
+});
+
+// Filtros
+const actualizarFiltros = ()=>{
+    divImg.style.filter = `brightness(${brightness.value}) 
+    opacity(${opacity.value}) contrast(${contrast.value}%) 
+    blur(${blur_.value}px) grayscale(${grayscale.value}%) 
+    sepia(${sepia.value}%) hue-rotate(${hue.value}deg) 
+    saturate(${saturation.value}%) invert(${negative.value})`;
+}
+brightness.addEventListener("change", actualizarFiltros);
+opacity.addEventListener("change", actualizarFiltros);
+contrast.addEventListener("change", actualizarFiltros);
+blur_.addEventListener("change", actualizarFiltros);
+grayscale.addEventListener("change", actualizarFiltros);
+sepia.addEventListener("change", actualizarFiltros);
+hue.addEventListener("change", actualizarFiltros);
+saturation.addEventListener("change", actualizarFiltros);
+negative.addEventListener("change", actualizarFiltros);
+
+// Reestablecer filtros
+
+resetFilters.addEventListener("click", () => {
+    brightness.value = "1";
+    opacity.value = "1";
+    contrast.value = "1000";
+    blur_.value = "0";
+    grayscale.value = "0";
+    sepia.value = "0";
+    hue.value = "0";
+    saturation.value = "100";
+    negative.value = "0";
+    actualizarFiltros();
+});
+
+
+
+
+
+
+
+
+
 
